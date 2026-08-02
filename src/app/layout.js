e4b96383bbcf95db7b3549a32a3ee3ddb1cf0fbc
lizadeali02 <elielizade1020@gmail.com>
2026-08-02 11:46:07 -0700

    add the changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { LanguageProvider } from "context/LanguageContext";
+import { LanguageProvider } from "@/context/LanguageContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
